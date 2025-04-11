e76e5e7748013652ba6ce251857c5a3e674e4c22
GoogolplexJ <jared@selkefamily.com>
2025-04-11 17:33:09 -0500

    Creation update 2

---
+++ scripts/MenuButtonScripts/song_creation.gd
@@ -0,0 +1,12 @@
+extends Control
+class_name SongCreation
+
+
+
+
+func _on__toggled(toggled_on: bool, extra_arg_0: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_item_selected(index: int, extra_arg_0: int) -> void:
+	pass # Replace with function body.
