b167d66f24f96f75119b03582728422e361c14e4
GoogolplexJ <jared@selkefamily.com>
2025-04-24 16:19:29 -0500

    Creation update

---
+++ scripts/FileWriter.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using System.IO;
+
+public partial class FileWriter : Node
+{
+    public StreamWriter sW;
+    string exeFolder = System.IO.Path.GetDirectoryName("Guitar Kaizen/Assets/Song Text Files");
+    public FileWriter(){
+    }
+    public void CreateSW(string str, bool append){
+        string s = exeFolder + str + ".txt";
+        sW = new StreamWriter(s, append);
+    }
+    public void Append(string str){
+        sW.Write(str);
+    }
+}
