4619a18eb2abcd072eec96967d7e23b9d0611e95
GoogolplexJ <jared@selkefamily.com>
2025-04-27 20:57:38 -0500

    hiiiiii added :)

--- scripts/MenuScripts/song_creation.gd
+++ scripts/MenuScripts/song_creation.gd
@@ -1,6 +1,5 @@
 extends Control
 class_name SongCreation
-const FileWriter := preload("res://scripts/FileWriter.cs")
 
 var toggleArray = []
 var itemArray = []
@@ -9,18 +8,19 @@ var botText = ""
 var bpm = ""
 var barLength = ""
 var barType = 0 
-var fileName = ""
-var fW := FileWriter.new()
-var initialized = false
-var file = FileAccess.open(save_path, FileAccess.WRITE)
-var folder_path := "user://SongFiles"
-var save_name := "noNameSong.ini"
+var noteLength = 2
+var folder_path := "user://SongFiles/"
+var save_name := "noNameSong.dat"
 var save_path := folder_path + save_name
+var file = FileAccess.open(save_path, FileAccess.WRITE)
 
 # when scene starts make sure variables are at default,
 # then begin the text file editing so that song can be created
 func _ready():
 	_blank_variables()
+	var dir = DirAccess.open("user://")
+	if(!dir.dir_exists("SongFiles")):
+		dir.make_dir("SongFiles")
 	
 
 func _input(event):
@@ -40,7 +40,7 @@ func _blank_variables() -> void:
 	bpm = ""
 	barLength = ""
 	barType = 0 
-	fileName = ""
+	noteLength = 2
 
 # List Based Buttons, changes index in list to new value 
 # based on which note changed
@@ -48,6 +48,9 @@ func _on__toggled(toggled_on: bool, extra_arg_0: int) -> void:
 	toggleArray[extra_arg_0] = toggled_on
 func _on_option_button_item_selected(index: int, extra_arg_0: int) -> void:
 	itemArray[extra_arg_0] = index
+
+func _on_note_length_item_selected(index: int) -> void:
+	noteLength = index
 
 # unselects all buttons back to default value
 func _unselect_buttons() -> void:
@@ -121,35 +124,72 @@ func _on_top_signature_text_changed() -> void:
 	barLength = $TimeSignatures/TopSignature.text
 
 
+# file writing section
+# negatives are fake and actually wrap around to 255
+# so Note: 255, BPM: 254, TimeSignature: 253, Bars: 252
+
+# -1 indicates Note
 func _on_make_note_pressed() -> void:
+	file.store_8(-1)
+	file.store_8(noteLength)
+	for n in 24:
+		if(toggleArray[n]):
+			file.store_8(-1)
+			file.store_8(note_format_change(n, itemArray[n]))
+			file.store_8(itemArray[n])
 	_unselect_buttons()
 	_blank_variables()
 
+func note_format_change(value : int, sharpFlat : int) -> int:
+	if value == 0:
+		return 0
+	var newValue := value
+	if not (value <= 9): newValue -= 1
+	if not (value <= 20): newValue -= 1
+	if not (value <= 32): newValue -= 1
+	newValue = (newValue - 1) * 2
+	#adjust flats and sharps to their corresponding base note value
+	match sharpFlat:
+		1:
+			newValue += 1
+		2: 
+			newValue -= 1
+	return newValue
 
+# -4 indicates Bar
 func _on_make_bars_pressed() -> void:
+	file.store_8(-4)
+	file.store_8(int(barLength))
 	_unselect_buttons()
 	_blank_variables()
 
-
+# -2 indicates BPM
 func _on_make_bpm_pressed() -> void:
+	file.store_8(-2)
+	file.store_8(int(bpm))
 	_unselect_buttons()
 	_blank_variables()
 
-
+# -3 indicates Time Signature
 func _on_make_time_pressed() -> void:
+	file.store_8(-3)
+	file.store_8(int(topText))
+	file.store_8(int(botText))
 	_unselect_buttons()
 	_blank_variables()
 
 
 func _on_overwrite_pressed() -> void:
+	file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.resize(0)
 	_unselect_buttons()
 	_blank_variables()
-	initialized = true
 
 func _on_append_pressed() -> void:
+	file = FileAccess.open(save_path, FileAccess.WRITE)
 	_unselect_buttons()
 	_blank_variables()
-	initialized = true
 
 func _on_name_text_changed() -> void:
-	fileName = $Create/Name.text
+	save_name = $Create/Name.text
+	save_path = folder_path + save_name
